692ab3fc746ee6ebf51b93d4fff541a4f5fa3b35
Admin <enfros2000@gmail.com>
2021-09-17 15:28:01 +0300

    Finish simple renderer

--- src/components/Elementor/ChildRenderer.js
+++ src/components/Elementor/ChildRenderer.js
@@ -1,23 +1,31 @@
 import * as React from "react"
 import ucwordfirst from "../../utils/ucwordfirst"
 import * as components from "./index"
-
+import Section from './Section';
+import Column from "./Column";
 const elementorComponents = {
-    Section: components.Section,
-    Column: components.Column,
+    Section: Section,
+    Column: Column,
 }
 const ChildRenderer = ({ elements, id }) => {
+    console.log('there', elements);
     return (
         <div key={id} >
             {
                 elements.map(item => {
                     console.log(item)
-                    return React.createElement(
-                        elementorComponents[ucwordfirst(item.elType)],
-                        {
-                            ...item
-                        }
-                    )
+                    if (elementorComponents[ucwordfirst(item.elType)])
+                        return React.createElement(
+                            elementorComponents[ucwordfirst(item.elType)],
+
+                            {
+                                id: item.id,
+                                ...item
+                            }
+                        )
+                    else {
+                        return (<h5>Not implemented yet {item.widgetType}</h5>)
+                    }
                 }
                 )
             }
